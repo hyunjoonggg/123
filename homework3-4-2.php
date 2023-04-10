1004c8230b2bd6641efedec3868d2e8e5682dac5
hyunjoonggg <127706406+hyunjoonggg@users.noreply.github.com>
2023-04-10 11:35:57 +0900

    Add files via upload

---
+++ homework3-4-2.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+사각형의 넓이<br>
+
+box area = <?php echo $_POST["width"]*$_POST["height"]; ?><br>
+
+삼각형의 넓이<br>
+tkarkrgud = <?php echo ($_POST["width"]*$_POST["height"]/2); ?>
+
+</body>
+</html>
